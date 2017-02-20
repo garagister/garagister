2d24f1dba44c1ff89c857d8b2e6f16980ae18175
jerome <fanniojerome@free.fr>
2017-02-20 17:25:13 +0100

    recup

--- db/migrate/20170220145844_create_garagistes.rb
+++ db/migrate/20170220145844_create_garagistes.rb
@@ -2,7 +2,8 @@ class CreateGaragistes < ActiveRecord::Migration[5.0]
   def change
     create_table :garagistes do |t|
       t.string :name
-      t.stringadresse :ville
+      t.string :ville
+      t.string :adresse
       t.integer :code_postal
       t.string :categorie
       t.decimal :taux_mo
