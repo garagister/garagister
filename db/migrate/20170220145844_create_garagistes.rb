7c78246477e697625c3e1bf797e8ce2ec47eb619
Horsell <ihorsell@protonmail.ch>
2017-02-20 17:21:58 +0000

    correction string model create_garagister.rb

--- db/migrate/20170220145844_create_garagistes.rb
+++ db/migrate/20170220145844_create_garagistes.rb
@@ -2,7 +2,7 @@ class CreateGaragistes < ActiveRecord::Migration[5.0]
   def change
     create_table :garagistes do |t|
       t.string :name
-      t.stringadresse :ville
+      t.string :ville
       t.integer :code_postal
       t.string :categorie
       t.decimal :taux_mo
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170220145844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "garagistes", force: :cascade do |t|
+    t.string   "name"
+    t.string   "ville"
+    t.integer  "code_postal"
+    t.string   "categorie"
+    t.decimal  "taux_mo"
+    t.decimal  "taux_piece"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
