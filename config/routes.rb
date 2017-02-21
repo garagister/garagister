85712ece42d1229267e2b2c742b8ac6b953737bb
Horsell <ihorsell@protonmail.ch>
2017-02-21 12:54:19 +0000

    delete roads in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,4 @@ Rails.application.routes.draw do
   resources :garagistes
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # rdv
-  post "search", to: "search#create"
-  get "search/:id", to: "search#show"
-
-  # rdv appointment
-  get "search/:id/appointment", to: "search#edit"
-  post "search/:id/appointment", to: "search/update"
 end
