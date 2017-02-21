c976e0a65f16ef0e531d58e6ce4a50a0cc6eef2d
Horsell <ihorsell@protonmail.ch>
2017-02-21 12:48:36 +0000

    add roads search && appointment

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   resources :garagistes
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # rdv
+  post "search", to: "search#create"
+  get "search/:id", to: "search#show"
+
+  # rdv appointment
+  get "search/:id/appointment", to: "search#edit"
+  post "search/:id/appointment", to: "search/update"
 end
