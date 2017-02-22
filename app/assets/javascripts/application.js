17d88e4a43c9f05419ce575446bb5cb7f846df8a
Claire Durand-Viel <PetitChat@MacBook-Air-de-Claire.local>
2017-02-22 16:00:56 +0100

    form created

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require select2-full
 //= require_tree .
-//= require select2
--- app/assets/javascripts/select2.js
+++
@@ -1,6 +0,0 @@
-$( ".select2" ).select2({
-    theme: "bootstrap"
-});
-
-
-//f.select :city, collection: interventions_collection,  class: "select2"
