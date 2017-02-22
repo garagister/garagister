a3b668b974885ac80855f04497d87875e615e9fb
jerome13011 <fanniojerome@free.fr>
2017-02-22 18:14:04 +0100

    resolution conflit routes pour home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :garagistes
-  root to: 'page#home'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :searches, only:[:create, :show]
