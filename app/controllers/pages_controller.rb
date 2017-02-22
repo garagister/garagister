1a5ac3345f0b3956c20fe4c12182a065bdc9df62
jerome13011 <fanniojerome@free.fr>
2017-02-22 17:11:14 +0100

    modif formulaire index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-
+    @search = Search.new
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222110635) do
+ActiveRecord::Schema.define(version: 20170222160042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,8 +42,9 @@ ActiveRecord::Schema.define(version: 20170222110635) do
     t.integer  "code_postal"
     t.string   "categorie"
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "intervention"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "villes", force: :cascade do |t|
